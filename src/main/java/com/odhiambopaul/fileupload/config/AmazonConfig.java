c54095effa06a0a27e0e9ef90b7fd96a5e73caaa
paul odhiambo <paulodhiambo962@gmail.com>
2020-12-29 20:36:01 +0300

    Update AmazonConfig.java

--- src/main/java/com/odhiambopaul/fileupload/config/AmazonConfig.java
+++ src/main/java/com/odhiambopaul/fileupload/config/AmazonConfig.java
@@ -13,7 +13,7 @@ public class AmazonConfig {
     @Bean
     public AmazonS3 s3() {
         AWSCredentials awsCredentials =
-                new BasicAWSCredentials("AKIAI356LU7ITLVGZMDQ", "R9+828XO9z22rM7j7P3WdETAynPrPI30PbjsT8jO");
+                new BasicAWSCredentials("", "");
         return AmazonS3ClientBuilder
                 .standard()
                 .withRegion("ap-south-1")
